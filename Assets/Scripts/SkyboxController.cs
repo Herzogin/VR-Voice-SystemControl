e4a1f4350b8102fcd38c1605599fe19b392cffcf
Nele <nuell@cyl.de>
2021-04-01 19:12:43 +0200

    added Day-Night change

---
+++ Assets/Scripts/SkyboxController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxController : MonoBehaviour
+{
+
+    public Material daySky;
+    public Material nightSky;
+
+
+    public void SkyToDay()
+    {
+        RenderSettings.skybox = daySky;
+    }
+
+    public void SkyToNight()
+    {
+        RenderSettings.skybox = nightSky;
+    }
+}
--- Assets/Scripts/VoiceCommands.cs
+++ Assets/Scripts/VoiceCommands.cs
@@ -10,11 +10,17 @@ public class VoiceCommands : MonoBehaviour
     KeywordRecognizer keywordRecognizer;
     Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
     GameObject sceneManager;
+    //public GameObject turtle;
+    //TurtleMove TurtleScript;
+    public GameObject kamera;
+    SkyboxController skyboxScript;
     
 
     void Start()
     {
         sceneManager = GameObject.Find("SceneManager");
+        //TurtleScript = GameObject.FindObjectOfType(typeof(TurtleMove)) as TurtleMove;
+        skyboxScript = GameObject.FindObjectOfType(typeof(SkyboxController)) as SkyboxController;
 
         //Create keywords for keyword recognizer
         keywords.Add("Anfang", () =>
@@ -46,6 +52,40 @@ public class VoiceCommands : MonoBehaviour
             Debug.Log("Hilfe aus");
             GameObject.Find("MainCanvas").GetComponent<Canvas>().enabled = false;
         });
+
+        keywords.Add("Stop", () =>
+        {
+            Debug.Log("Stop");
+            GameObject.Find("Butterfly").GetComponent<Animation>().enabled = false;
+        });
+
+        keywords.Add("Start", () =>
+        {
+            Debug.Log("Start");
+            GameObject.Find("Butterfly").GetComponent<Animation>().enabled = true;
+        });
+
+        keywords.Add("Tag", () =>
+        {
+            Debug.Log("Tag");
+            skyboxScript.SkyToDay();
+        });
+
+        keywords.Add("Nacht", () =>
+        {
+            Debug.Log("Nacht");
+            skyboxScript.SkyToNight();
+        });
+
+        //keywords.Add("Schildkröte", () =>
+        //{
+        //    Debug.Log("Schildkröte");
+        //    TurtleScript.TurtleWalk();
+        //});
+
+
+
+
 
         keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
 
